7c36d31a974d76c18685cc24c4e7119a13a3da0b
Casey Idsinga <cidsinga@gmail.com>
2022-04-19 19:56:44 -0700

    added turn taking via xIsNext in Board

--- src/index.js
+++ src/index.js
@@ -2,16 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square" 
-      onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props){
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
@@ -19,15 +15,18 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'x';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({squares: squares,
+    xIsNext: !this.state.xIsNext,
+    });
   }
-  
+
   renderSquare(i) {
     return (
     <Square 
